5e28b53251d6e08cd2d368a0da56217a9ca93cd1
A-Potat0 <166179776+A-Potat0@users.noreply.github.com>
2025-10-03 16:30:49 -0400

    made secret disco button and a way of knowing
    the mouse turns invisible when you hover over the disco section that will act differently than if you press elsewhere

--- template-p5-project/js/script.js
+++ template-p5-project/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * window to the soul
+ * Anthony Patient
  * 
  * the eye is interactible with a mouse try mouving arround or clicking
  * 
@@ -18,7 +18,15 @@ let blinking1 = 35;
 let blinking2 = undefined;
 //these variables are for the blinking
 let press = 0
-let timer = 6.3 * 1000; // 3 seconds
+let timer = 6.20 * 1000; // 3 seconds
+//these are the base variables that will let me randomize the color
+let randomHue = 100;
+let randomSat = 50;
+let randomBris = 50;
+//this will let me make the herart disco
+let randomHeartHue = 255;
+//this variable is used to make a disco background (it is appropriate and somewhat descriptive :\ )
+let disco = 0
 
 /**
  * sets up the canvase and important details of the peice
@@ -34,20 +42,16 @@ function setup() {
     // Set the color mode to hue-saturation-brightness (HSB)
     colorMode(HSB);
 
-
 }
 
 
-
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * this draws the whole project calling the different functions needed
 */
 function draw() {
-    //background(160, 180, 200);
 
-    // Clear the background
-    background(0, 0, 0);
+    // set the background color
+    background(randomHue / 2, randomSat / 2, randomBris / 2);
 
     eye();
 
@@ -55,33 +59,75 @@ function draw() {
 
     heart();
 
-    blink()
+    blink();
 
     //pulses the eye color
     brightness = map(sin(frameCount * 0.05), -1, 1, 85, 150);
     saturation = map(sin(frameCount * 0.9), -1, 1, 50, 400);
 
+    if ((mouseX <= (width / 2) + 25 && mouseX >= (width / 2) - 25) && (mouseY <= (height / 2) + 33 && mouseY >= (height / 2) - 29)) {
+        cursor('none');
+    }
+    else if (mouseX < (width / 2) && mouseY < (height / 2)) {
+        cursor('ne-resize');
+    }
+    else if (mouseX == (width / 2) && mouseY <= (height / 2)) {
+        cursor('e-resize');
+    }
+    else if (mouseX > (width / 2) && mouseY < (height / 2)) {
+        cursor('se-resize');
+    }
+    else if (mouseX >= (width / 2) && mouseY == (height / 2)) {
+        cursor('s-resize');
+    }
+    else if (mouseX > (width / 2) && mouseY > (height / 2)) {
+        cursor('sw-resize');
+    }
+    else if (mouseX == (width / 2) && mouseY >= (height / 2)) {
+        cursor('w-resize');
+    }
+    else if (mouseX < (width / 2) && mouseY > (height / 2)) {
+        cursor('nw-resize');
+    }
+    else if (mouseX <= (width / 2) && mouseY == (height / 2)) {
+        cursor('n-resize');
+    }
+    else {
+        cursor(AUTO);
+    }
     //checks if the mouse was clicked before it runs the code
-    if (press == 1) {
+    if (disco == 1) {
+        //makes the background go disco
+        randomHue = random(2, 360);
+        randomSat = random(2, 100);
+        randomBris = random(2, 100);
+    }
 
+    else if (press == 1) {
+
+        //this is the blinking
         blinking1 = map(cos(frameCount), -1, 1, 0, 35);
         blinking2 = map(cos(frameCount), -1, 1, 0, 12);
+        //changes the color of the heart :)
+        randomHeartHue = map(sin(random(random(0, 180) * mouseX * 0.01, random(180, 360) * mouseY * 0.01)), -1, 1, 0, 360)
 
     }
 
     else if (press === 0) {
+        //reset to base values the blink location and heart color
         blinking1 = 35;
         blinking2 = 12;
+        randomHeartHue = 255;
     }
 
     // im just using this to see usfull info
-    print(press)
+    print(randomHeartHue)
 }
 
 // makes a heart shape
 function heart() {
     noStroke();
-    fill(255, saturation, brightness);
+    fill(randomHeartHue, saturation, brightness);
     triangle((width / 2) + ((mouseX - width / 2) / 8), (height / 2 + 33) + (mouseY - height / 2) / 8, (width / 2 + 24.3) + ((mouseX - width / 2) / 8), (height / 2 - 10) + (mouseY - height / 2) / 8, (width / 2 - 24.3) + ((mouseX - width / 2) / 8), (height / 2 - 10) + (mouseY - height / 2) / 8);
     ellipse((width / 2 + 12) + ((mouseX - width / 2) / 8), (height / 2 - 15) + (mouseY - height / 2) / 8, 26);
     ellipse((width / 2 - 12) + ((mouseX - width / 2) / 8), (height / 2 - 15) + (mouseY - height / 2) / 8, 26);
@@ -89,6 +135,7 @@ function heart() {
 
 // makes the shine arround the heart (i call them spikes)
 function radiant() {
+    //a part of this was copied from https://p5js.org/examples/transformation-rotate/
     // Loop through angles 0, 30, 60, 90 degrees
     for (let angle = 0; angle <= 360; angle += 30) {
         // Save current coordinate system
@@ -145,26 +192,46 @@ function eye() {
 // moves the eyelids
 function blink() {
     translate((width / 2) + ((mouseX - width / 2) / 20), (height / 2) + (mouseY - height / 2) / 20);
-    fill(2, 2, 2);
+    fill(randomHue, randomSat, randomBris);
     arc(0, 40, 650, 332, blinking1 + 180, -blinking1 + 0, OPEN);
     arc(0, 40, 650, 162, blinking2 + 0, -blinking2 + 180, OPEN);
-
 
 }
 
 // changes the variable to false if the timer ends ( this is for press ((for now)))
 function tiemrEnd() {
+    // press = false
     press = 0;
+
+    //set random color for background
+    randomHue = random(2, 360);
+    randomSat = random(2, 100);
+    randomBris = random(2, 100);
+
+    //disco = false
+    disco = 0
+
 }
 
 // check ifthe mouse was pressed to activate other code
 function mousePressed() {
 
     if (press === 0) {
+        //reset frameCount
         frameCount = 1;
+        //set press = true
         press = 1;
+
     }
     setTimeout(tiemrEnd, timer);
 
+}
+
+
+function mouseClicked() {
+    if ((mouseX <= (width / 2) + 25 && mouseX >= (width / 2) - 25) && (mouseY <= (height / 2) + 33 && mouseY >= (height / 2) - 29)) {
+        //disco = true
+        disco = 1
+    }
 
 }
